28adeb9211020ec4b91e158a61cfcf74ba12d2f4
FraneKuzmanic <frane.kuzmanic9@gmail.com>
2024-10-24 23:41:54 +0200

    adress changed on client

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://web2-lab1-1.onrender.com',
     timeout: 1000,
     headers: { 'Content-Type': 'application/json' },
 });
